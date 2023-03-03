aa11594894ec1957962a295c3f8d1dd5eb759eae
ismail <ismail.syedy@gmail.com>
2023-03-02 23:25:07 -0700

    completed

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+
+const cats =[ "Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+cats.push(name);
+return cats;
+}
+function destructivelyPrependCat(name){
+cats.unshift(name);
+return cats;
+
+}
+function destructivelyRemoveLastCat(){
+cats.pop();
+return cats;
+}
+function destructivelyRemoveFirstCat(){
+cats.shift();
+return cats;
+}
+
+function appendCat(name){
+const cat = [...cats,name];
+return cat;
+}
+function prependCat(name){
+ const cat =[name,...cats]   
+ return cat;
+
+}
+function removeLastCat(){
+  const removeCats =cats.slice(0,cats.length-1);
+  return removeCats;
+}
+function removeFirstCat(){
+const removecat= cats.slice(1);
+return removecat;
+}
